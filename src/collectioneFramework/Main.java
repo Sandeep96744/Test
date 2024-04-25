77fc82c80da19959f5729753047babd222aec9c8
Sandeep Sharma <sandeepsharma96744@gmail.com>
2024-04-25 11:31:00 +0530

    Added file in collectionFramework

--- src/collectioneFramework/Main.java
+++ src/collectioneFramework/Main.java
@@ -2,40 +2,44 @@ package collectioneFramework;
 import java.util.*;
 
 public class Main {
-    Queue<Task> q;
-
+    Queue<Task> taskQueue;
+    Stack<Task> taskCompleted;
     public Main() {
-        q = new LinkedList<Task>();
+        taskQueue = new LinkedList<>();
+        taskCompleted = new Stack<>();
     }
 
     void addNewTask(Task task) {
-        q.offer(task);
+        taskQueue.offer(task);
+        System.out.println("Task Added:\n" + task);
     }
 
-    Stack<Task> processTasks() {
-        Stack<Task> st = new Stack<>();
+    void processTasks() {
 
-        while(!q.isEmpty()) {
-            Task task = q.poll();
+        while(!taskQueue.isEmpty()) {
+            Task task = taskQueue.poll();
+            System.out.println("Processing Task: " + task.description);
             task.status = "completed";
-            st.push(task);
+            System.out.println("Processed!");
+            taskCompleted.push(task);
         }
-        return st;
     }
 
-    void displayCompletedTask(Stack<Task> st) {
-        if(st.isEmpty()) {
-            System.out.println("None of the task completed");
+    void displayCompletedTask() {
+        if(taskCompleted.isEmpty()) {
+            System.out.println("None of the task completed.");
             return;
         }
-        System.out.println("Completed Task:\n--------------------------");
-        while(!st.isEmpty()) {
-            Task task = st.pop();
+        System.out.println("Completed Tasks:\n--------------------------");
+        while(!taskCompleted.isEmpty()) {
+            Task task = taskCompleted.pop();
             System.out.println(task);
         }
     }
 
     public static void main(String[] args) {
+        System.out.println("Welcome to ToDo-Manager\n--------------------------");
+
         Task t1 = new Task("Hit the Gym", 30);
         Task t2 = new Task("Read books", 60);
         Task t3 = new Task("Pay Bills", 15);
@@ -46,9 +50,9 @@ public class Main {
         m.addNewTask(t2);
         m.addNewTask(t3);
 
-        Stack<Task> st = m.processTasks();
+        m.processTasks();
 
-        m.displayCompletedTask(st);
+        m.displayCompletedTask();
 
     }
 }
